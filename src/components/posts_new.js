2aff5710a182678a4298c43ee6145b99b324a01e
Anna Golasowska <annagolasowska@gmail.com>
2018-03-31 16:34:40 +0200

    added post method

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -1,19 +1,30 @@
 import React, { Component } from 'react';
 import { Field, reduxForm } from 'redux-form';
+import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+import { createPost } from '../actions';
 
 class PostsNew extends Component {
   renderField(field) {
+    const { meta: { touched, error } } = field;
+    const className = `form group ${touched && error ? 'has-danger' : ''}`;
     return (
-      <div className="form-group">
+      <div className={className}>
         <label>{field.label}</label>
         <input type="text" className="form-control" {...field.input} />
+        <div className="text-help">{touched ? error : ''}</div>
       </div>
     );
   }
 
+  onSubmit = values => {
+    this.props.createPost(values, () => this.props.history.push('/'));
+  };
+
   render() {
+    const { handleSubmit } = this.props;
     return (
-      <form>
+      <form onSubmit={handleSubmit(this.onSubmit)}>
         <Field label="Title" name="title" component={this.renderField} />
         <Field
           label="Categories"
@@ -25,11 +36,32 @@ class PostsNew extends Component {
           name="content"
           component={this.renderField}
         />
+        <button type="submit" className="btn btn-primary">
+          Add
+        </button>
+        <Link to="/" className="btn btn-danger">
+          Cancel
+        </Link>
       </form>
     );
   }
 }
 
+function validate(values) {
+  const errors = {};
+  if (!values.title || values.title.length < 3) {
+    errors.title = 'Enter a title that is at least 3 characters!';
+  }
+  if (!values.categories) {
+    errors.categories = 'Enter some categories!';
+  }
+  if (!values.content) {
+    errors.content = 'Enter some content!';
+  }
+  return errors;
+}
+
 export default reduxForm({
+  validate,
   form: 'PostsNewForm'
-})(PostsNew);
+})(connect(null, { createPost })(PostsNew));
